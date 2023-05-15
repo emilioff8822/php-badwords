a2a1e08dd798c626270f9748663a641b328ad025
emilioff8822 <emilioff@hotmail.it>
2023-05-15 13:52:52 +0200

    commit-atterraggio-replace-length

---
+++ atterraggio.php
@@ -0,0 +1,28 @@
+<?php
+
+$paragrafo = $_POST["paragrafo"];
+$parola =  $_POST ["parola"];
+
+$paragrafo_censurato = str_replace( $parola, '***', $paragrafo);
+
+?>
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Atterraggio</title>
+</head>
+<body>
+
+<h1>ciao</h1>
+ <p>Paragrafo Originale: <?php echo $paragrafo; ?></p> 
+ <p> Lunghezza Paragrafo Originale: <?php echo strlen($paragrafo); ?></p> 
+
+ <p>Paragrafo censurato: <?php echo $paragrafo_censurato; ?> </p>
+ <p> Lunghezza Paragrafo censurato: <?php echo strlen($paragrafo_censurato); ?> </p>
+</body>
+</html>
