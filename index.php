74ad67d9a0fc1e7a54ce9a28de61407a9a2be2ca
emilioff8822 <emilioff@hotmail.it>
2023-05-15 13:33:10 +0200

    commit-indexphp

---
+++ index.php
@@ -0,0 +1,27 @@
+<?php
+
+?>
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Document</title>
+</head>
+<body>
+  <form action="atterraggio.php" method="post">
+  <label for="paragrafo"> Inserisci il paragrafo</label> <br>
+  <textarea name="paragrafo" id="paragrafo" cols="50" rows="2"></textarea> <br>
+
+  <label for="parola">Inserisci la Parola da Censurare</label> <br>
+  <input type="text" id="parola" name="parola"> <br>
+<button type="submit"> Invia</button>
+
+
+  </form>
+  
+</body>
+</html>
